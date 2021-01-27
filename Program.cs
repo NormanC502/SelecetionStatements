d953e8ef6daa07f34fd8b00089504bdbbb66fc37
Norman Calhoun <shakauluz73@yahoo.com>
2021-01-27 15:17:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 5;
+
+            Console.WriteLine("What number are you thinking of? Hint it's in the middle somewhere!");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber) 
+            {
+                Console.WriteLine("Too Low, Try again!");
+            }
+            else if (userInput > favNumber) 
+            {
+                Console.WriteLine("Too High, Try again!");
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("You know me, Hooray!");
+            }
+            else
+            {
+                Console.WriteLine("Never Mind, you didn't get it!");
+            }            
+        }
+    }
+}
